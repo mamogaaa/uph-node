3f978b96dd78b074f6b4156ab9b23488fc4cbca3
nordikafiles <maksiik@gmail.com>
2017-08-10 17:23:38 +0300

    fixes

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
   _while: require('./components/while.js'),
   _sleep: require('sleep-promise'),
   _for: require('./components/for.js'),
+  _forEach: require('./components/forEach.js'),
 };
