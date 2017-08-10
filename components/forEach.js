ed1ed2ee976fc2095af1188a4cd130b76f9d019f
nordikafiles <maksiik@gmail.com>
2017-08-10 17:18:11 +0300

    fixes

---
+++ components/forEach.js
@@ -0,0 +1,17 @@
+function _forEach(array cb, current, ret) {
+  if (current == undefined) {
+    current = 0;
+  }
+  if (ret == undefined) {
+    ret = [];
+  }
+  return cb(current)
+  .then(function(res){
+    current++
+    ret.push(res)
+    if (current <= array.length-1) return _forEach(from, to, cb, array[current], ret);
+    return Promise.resolve(ret);
+  });
+};
+
+module.exports = _forEach;
